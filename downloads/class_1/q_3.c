bd3ca5cce0934a47e75bed3a93085734c291bda9
leehao0709 <144991606+leehao0709@users.noreply.github.com>
2024-01-10 18:23:13 +0000

    已新增C_ex中的Class 1中的第三題

---
+++ downloads/class_1/q_3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+float a = 0.5;
+double b = 1.2;
+int c = 3;
+b = b + a + c;
+
+/* 輸出 a, b, c 到螢幕 */
+printf("a = %3.1f, b = %3.1f, c = %d\n", a, b, c);
+
+return 0;
+}
