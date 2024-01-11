93d60bc13251d7dc1835d338bf4feb59aef50b64
leehao0709 <144991606+leehao0709@users.noreply.github.com>
2024-01-11 03:56:35 +0000

    已新增ANSIC中的課程內容其7

--- downloads/A_Class2/cp-7.c
+++ downloads/A_Class2/cp-7.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 
-int checkOddEven(int n1)
-{   
-    return (n1 & 1);
+int fact(int);
+
+int main() {
+    int sum;
+    // 計算數列總和：1!/1 + 2!/2 + 3!/3 + 4!/4 + 5!/5
+    sum = fact(1) / 1 + fact(2) / 2 + fact(3) / 3 + fact(4) / 4 + fact(5) / 5;
+
+    printf("\n\n 函式：計算數列 1!/1 + 2!/2 + 3!/3 + 4!/4 + 5!/5 的總和 :\n");
+    printf("----------------------------------------------------------\n");
+
+    printf("數列總和為 : %d\n\n", sum);
+    return 0;
 }
 
-int main()
-{
-    int n1;
-    int scanfResult; // 儲存 scanf 函式的返回值
+// 計算階乘的函式
+int fact(int n) {
+    int num = 0, f = 1;
 
-    printf("\n\n 函式：檢查數字是奇數還是偶數：\n");
-    printf("------------------------------------------------\n");
-
-    printf("輸入任意數字：");
-    scanfResult = scanf("%d", &n1); // 儲存 scanf 函式的返回值
-
-    if (scanfResult != 1) {
-        printf("無效的輸入。\n");
-        return 1; // 適當的錯誤處理
+    // 計算階乘
+    while (num <= n - 1) {
+        f = f + f * num; // 這裡的階乘計算方式可能不正確
+        num++;
     }
-
-    if(checkOddEven(n1))
-    {
-        printf("輸入的數字是奇數。\n\n");
-    }
-    else
-    {
-        printf("輸入的數字是偶數。\n\n");
-    }
-    return 0;
+    return f;
 }
