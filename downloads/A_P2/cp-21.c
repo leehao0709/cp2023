e46ffee295546ff200518aabddf27c1a602f8000
leehao0709 <144991606+leehao0709@users.noreply.github.com>
2024-01-11 04:26:37 +0000

    已新增ANSIC中的EX_2 第1題

---
+++ downloads/A_P2/cp-21.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()  
+{
+    // Print Name
+    printf("Name   : Andy\n"); 
+
+    // Print Date of Birth
+    printf("DOB    : July 30, 2049\n"); 
+
+    // Print Mobile Number
+    printf("Mobile : 07-7777777748\n"); 
+
+    // Indicate successful execution
+    return(0); 
+}
