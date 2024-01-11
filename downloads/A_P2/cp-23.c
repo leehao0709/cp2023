ab620040c5b9c874f4ec95985d9bc2e056f291f2
leehao0709 <144991606+leehao0709@users.noreply.github.com>
2024-01-11 04:35:13 +0000

    已新增ANSIC中的EX_2 第3題

---
+++ downloads/A_P2/cp-23.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() 
+{
+    // Print top line of pattern
+    printf("    ******\n");
+
+    // Print second line of pattern
+    printf("  **      **\n");
+
+    // Print lines 3 to 7 of pattern
+    printf(" *\n");
+    printf(" *\n");
+    printf(" *\n");
+    printf(" *\n");
+    printf(" *\n");
+
+    // Print bottom line of pattern
+    printf("  **      **\n");
+
+    // Print last line of pattern
+    printf("    ******\n");
+
+    return(0);
+}
